ae93876d6cee648eb3ce612662847d3cc294ace3
Raquel Saavedra <raquel.saavedra@unosquare.com>
2019-11-08 16:05:11 -0600

    Initial commit

---
+++ src/js/components/genre-menu-component.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class GenreMenu extends React.Component {
+    constructor() {
+        super()
+
+    }
+
+    // getGenres() {
+        
+    // }
+    render() {
+        return(
+            <div>Hola</div>
+        );
+    }
+
+}
+
+ReactDOM.render(<GenreMenu />, window.document.getElementById("root"))
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// const path = require("path");
+// module.exports = {
+// 	entry: {
+// 		home: "./src/js/pages/home.js"
+// 	},
+// 	output: {
+// 		path: path.resolve(__dirname, "assets/js"),
+// 	},
+// 	module: {
+// 		rules: [{
+// 			test: /\.js$/,
+// 			exclude: /(node_modules)/,
+// 			use: {
+// 				loader: "babel-loader"
+// 			}
+// 		}]
+// 	}
+// };
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+	entry: SRC_DIR + "/js/components/genre-menu-component.js",
+	output: {
+		path: DIST_DIR + "/app",
+		filename: "bundle.js",
+		publicPath: "/app"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?/,
+				include: SRC_DIR,
+				loader: "babel-loader",
+				query: {
+					presets: ["@babel/env", "@babel/react"]
+				}
+			}
+		]
+	}
+};
+
+module.exports = config
